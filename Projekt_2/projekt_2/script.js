5ac23ea674d9dbbafaa46cb21d6386bd1ce3553f
vtom24 <tomanova.veronika95@gmail.com>
2024-11-11 09:29:54 +0100

    Add files via upload

---
+++ Projekt_2/projekt_2/script.js
@@ -0,0 +1,15 @@
+const menuIcon = document.querySelector(".menu-icon");
+const menuList = document.querySelector("nav");
+const hamburgerIcon = document.querySelector(".fa-solid");
+
+menuIcon.addEventListener("click", () => {
+  if (hamburgerIcon.classList[1] === "fa-bars") {
+    hamburgerIcon.classList.add("fa-xmark");
+    hamburgerIcon.classList.remove("fa-bars");
+    menuList.style.display = "block";
+  } else {
+    hamburgerIcon.classList.add("fa-bars");
+    hamburgerIcon.classList.remove("fa-xmark");
+    menuList.style.display = "none";
+  }
+});
